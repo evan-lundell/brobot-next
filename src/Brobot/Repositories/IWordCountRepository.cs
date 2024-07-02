138082a7a7a4806f02b31e1ea3b2f4b5898d036f
evan-lundell <evan.lundell@gmail.com>
2024-07-01 20:58:56 -0500

    Skiasharp fix (#44)
    * fixed dependency injection
    
    * fixed skiasharp
    
    * fixed datetime calculation
    
    ---------
    
    Co-authored-by: Evan Lundell <elundell@kipsu.com>

--- src/Brobot/Repositories/IWordCountRepository.cs
+++ src/Brobot/Repositories/IWordCountRepository.cs
@@ -5,5 +5,5 @@ namespace Brobot.Repositories;
 
 public interface IWordCountRepository : IRepository<WordCountModel, int>
 {
-    Task<IEnumerable<WordCountDto>> GetWordCountsByChannelId(ulong channelId, int monthsBack = 1, int limit = 100);
+    Task<IEnumerable<WordCountDto>> GetWordCountsByChannelId(ulong channelId, DateOnly startDate, DateOnly endDate, int limit = 100);
 }
